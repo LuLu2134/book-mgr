85d56a612fdee54f2d178efabf3f60fee4087f71
LuLu2134 <1273441107@qq.com>
2021-11-28 10:47:28 +0800

    前端图书管理基础界面

--- book-mgr-fe/src/main.js
+++ book-mgr-fe/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import Antd from 'ant-design-vue';
+import SpaceBetween from './components/SpaceBetween/index.vue';
+
 
 import 'ant-design-vue/dist/antd.css';
 
@@ -10,4 +12,5 @@ createApp(App)
 .use(store)
 .use(router)
 .use(Antd)
+.component('space-between', SpaceBetween)
 .mount('#app');
